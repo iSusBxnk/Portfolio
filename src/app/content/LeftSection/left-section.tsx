cac9cdd5dea7c8537d41e2b5ef48273f63ea87c5
chayakorn phukhiao <chayakon@spacetrax.co>
2025-09-28 05:04:41 +0700

    fix padding

--- src/app/content/LeftSection/left-section.tsx
+++ src/app/content/LeftSection/left-section.tsx
@@ -41,7 +41,7 @@ export default function LeftSection({ menuItems, currentSection }: LeftSectionPr
       <div className="sticky top-24 grid lg:grid-rows-[35%_45%_20%] lg:h-[87vh] gap-4 lg:gap-0 mb-16 lg:mb-0">
        <div className="space-y-4 ">
        <Header />
-       <div className="md:ml-16 pt-16 md:pt-0 md:block flex justify-center items-center">
+       <div className="md:ml-16 pt-6 md:pt-0 md:block flex justify-center items-center">
        <Profile
           image={profile.image}
           title={profile.title}
