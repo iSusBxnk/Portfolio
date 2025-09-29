149d18808e7fcf94e43439dbd5f3f5fb937e1a01
chayakorn phukhiao <chayakon@spacetrax.co>
2025-09-29 15:08:45 +0700

    fix import

--- src/app/content/RightSection/education.tsx
+++ src/app/content/RightSection/education.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React, { useEffect } from 'react'
-import TextShimmer from '@/app/components/typography/text-shrim'
 
 interface AboutProps {
     addSectionId: (sectionId: string) => void;
